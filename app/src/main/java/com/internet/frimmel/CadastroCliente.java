a5ad21c7a3fc96a0bd18cf90e4e3db70500bcaab
camila-frimmel <127354836+camila-frimmel@users.noreply.github.com>
2023-10-19 23:01:16 -0300

    Update CadastroCliente

--- app/src/main/java/com/internet/frimmel/CadastroCliente.java
+++ app/src/main/java/com/internet/frimmel/CadastroCliente.java
@@ -4,10 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class CadastroCliente extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private EditText CadastroNome;
+    private EditText CadastroCPF;
+    private EditText CadastroEmail;
+    private EditText CadastroTelefone;
+    private Button Continua;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +30,22 @@ public class CadastroCliente extends AppCompatActivity {
         Button Continua = findViewById(R.id.ContinuarCadastro);
         Button Cancela = findViewById(R.id.CancelarCadastro);
 
+        FirebaseApp.initializeApp(this);
+        db = FirebaseFirestore.getInstance();
+
+
         Continua.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent cont = new Intent(getApplicationContext(), CadastroParte2.class);
-                startActivity(cont);
+                /*Intent cont = new Intent(getApplicationContext(), CadastroParte2.class);
+                startActivity(cont);*/
+                String nome = CadastroNome.getText().toString();
+                String cpf = CadastroCPF.getText().toString();
+                String email = CadastroEmail.getText().toString();
+                String telefone = CadastroTelefone.getText().toString();
+
+                salvarDadosNoFirestore (nome, cpf, email, telefone);
+
             }
         });
 
@@ -32,5 +58,27 @@ public class CadastroCliente extends AppCompatActivity {
         });
 
 
+    }
+
+    private void salvarDadosNoFirestore(String nome, String cpf, String email, String telefone) {
+        // Crie um novo documento na coleção "cliente" com os dados inseridos
+        db.collection("cliente")
+                .add(new DadosCliente(nome, cpf, email, telefone))
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // Sucesso: O documento foi criado com êxito
+                        String novoDocumentoId = documentReference.getId();
+                        // Você pode fazer algo com o novoDocumentoId, se necessário
+                        Intent cont = new Intent(getApplicationContext(), CadastroParte2.class);
+                        startActivity(cont);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Lidar com erros
+                    }
+                });
     }
 }
