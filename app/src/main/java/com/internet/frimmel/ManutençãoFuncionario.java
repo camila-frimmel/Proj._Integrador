2690e2af7bb8d2c977b0010849761f862a033553
Camila <127354836+camila-frimmel@users.noreply.github.com>
2023-11-19 18:58:38 -0300

    AGORA A LISTA FUCIONA

--- app/src/main/java/com/internet/frimmel/ManutençãoFuncionario.java
+++ app/src/main/java/com/internet/frimmel/ManutençãoFuncionario.java
@@ -2,14 +2,17 @@ package com.internet.frimmel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -18,6 +21,7 @@ public class ManutençãoFuncionario extends AppCompatActivity {
     private ListView listView;
     private ArrayAdapter<String> adapter;
     private ArrayList<String> funcionariosList;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +35,33 @@ public class ManutençãoFuncionario extends AppCompatActivity {
         listView.setAdapter(adapter);
 
         // Configurar o acesso ao Firestore
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
+
         db.collection("agenda")
                 .orderBy("Data", Query.Direction.ASCENDING) // Substitua "Data" pelo nome do seu campo de data
                 .orderBy("Horário", Query.Direction.ASCENDING) // Substitua "Horário" pelo nome do seu campo de horário
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        // Limpar a lista antes de adicionar os novos dados
+                        Log.d("Firestore", "Dados recuperados com sucesso");
                         funcionariosList.clear();
 
                         // Iterar sobre os documentos do Firestore e adicionar à lista
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             String horarioFuncionario = document.getString("Horário");
                             String dataFuncionario = document.getString("Data");
-                            String horarioEData = dataFuncionario + " - " + horarioFuncionario;
-                            funcionariosList.add(horarioEData);
+                            String Data = dataFuncionario;
+                            String horario = horarioFuncionario;
+                            funcionariosList.add(Data);
+                            funcionariosList.add(horario);
+
                         }
 
                         // Notificar o adaptador sobre as mudanças
                         adapter.notifyDataSetChanged();
                     } else {
-                        // Lidar com erros de leitura do Firestore
+                        Log.e("Firestore", "Erro ao recuperar dados", task.getException());
+                        Toast.makeText(ManutençãoFuncionario.this, "ERRO!", Toast.LENGTH_SHORT).show();
                     }
                 });
 
